c0e412bbd75c4ef87f810864174c3a9750de31ef
Tom Shih <tshih98@gmail.com>
2021-03-24 18:04:56 -0700

    added column view

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -2,7 +2,10 @@ from django.contrib import admin
 from users.models import BusinessCard
 # Register your models here.
 class BusinessCardAdmin(admin.ModelAdmin):
+    model = BusinessCard
+    list_display = ("user_id", "first_name", "last_name", "age", "birth_date", "job_title", "employer", "location", "email", "phone_number")
     pass
 
 admin.site.register(BusinessCard, BusinessCardAdmin)
+
 
