974307e5429d90e4773a3e9fee12eed2b53ed3c8
Tom Shih <tshih98@gmail.com>
2021-04-01 15:43:26 -0700

    migrate backend

--- backend/cards/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import BusinessCard
-
-class BusinessCardSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = BusinessCard
-        fields = ("user_id", "first_name", "last_name", "age", "birth_date", "job_title", "employer", "location", "email", "phone_number")
